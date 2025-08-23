efdfd6fc5b6090317b503be99c8400cc38282dc5
hello30190f <108202763+hello30190f@users.noreply.github.com>
2025-08-23 14:52:55 +0900

    save

---
+++ firstPrototype/frontend/onenote alternative frontend/src/modules/element.tsx
@@ -0,0 +1,32 @@
+export default class element{
+    ID          = ""
+    type        = ""
+    data        = null
+
+    // z-index included. Not 3D.
+    position = {
+        x: 0,
+        y: 0,
+        z: 0,
+    }
+
+    constructor(){
+        
+    }
+
+    onResize(){
+
+    }
+
+    onMove(){
+
+    }
+
+    render(){
+        return(
+            <>
+                <div className="NotImplemented">NotImplemented</div>
+            </>
+        )
+    }
+}
---
+++ firstPrototype/frontend/onenote alternative frontend/src/modules/window.tsx
@@ -0,0 +1,24 @@
+import type element from "./element"
+
+export default class window{
+    elements = []
+
+    constructor(){
+        
+    }
+
+    addElems(elem:element){
+        
+    }
+
+    show(){
+        return(
+            <>
+                <div className="window">
+
+                </div>
+            </>
+        )
+    }
+}
+
