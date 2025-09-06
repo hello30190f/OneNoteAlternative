353846c97a46c715e582510769c939ea396de156
hello30190f <108202763+hello30190f@users.noreply.github.com>
2025-09-06 17:47:32 +0900

    refactor

--- firstPrototype/frontend/onenote alternative frontend/src/modules/network/database.tsx
+++ firstPrototype/frontend/onenote alternative frontend/src/modules/network/database.tsx
@@ -9,42 +9,6 @@ type DatabaseState = {
   getWebsocket: () => WebSocket | null;
 };
 
-export function useDatabase() {
-    const [websocket, setWebsocket] = useState<WebSocket | null>(null);
-    const [serverIP, setServerIP] = useState<string>("ws://localhost:55225");
-
-    function getWebsocket() {
-        return websocket;
-    }
-
-    function connectDataServer(ip: string) {
-        setWebsocket(new WebSocket(ip));
-    }
-
-    function closeConnection() {
-        websocket?.close();
-    }
-
-    function changeServer(ip: string) {
-        setServerIP(ip);
-    }
-
-    useEffect(() => {
-        if (serverIP) connectDataServer(serverIP);
-
-        return () => {
-            websocket?.close();
-        };
-    }, [serverIP]);
-
-    return {
-        getWebsocket,
-        connectDataServer,
-        closeConnection,
-        changeServer,
-    };
-}
-
 export const useDatabaseStore = create<DatabaseState>((set, get) => ({
   websocket: null,
   serverIP: "ws://localhost:55225",
