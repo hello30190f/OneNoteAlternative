fcb829ed33c81656abae6f6cf087b7e386e22e26
hello30190f <108202763+hello30190f@users.noreply.github.com>
2025-09-18 15:19:10 +0900

    the data server backend implement

--- firstPrototype/backend/dataServer/commands/pageInfo.py
+++ firstPrototype/backend/dataServer/commands/pageInfo.py
@@ -4,9 +4,11 @@ from helper import loadSettings
 async def pageInfo(request,websocket):
     root = loadSettings.settings["NotebookRootFolder"]
 
-    path = request["path"]
-
-
-
+    pagePathFromContent = request["pageID"]
+    notebookName        = request["notebook"]
+    targetPath          = root + "/" + notebookName + "/contents/" + pagePathFromContent
+    
+    with open(targetPath,"rt") as aPage:
+        pass
 
     await NotImplementedResponse(websocket)
