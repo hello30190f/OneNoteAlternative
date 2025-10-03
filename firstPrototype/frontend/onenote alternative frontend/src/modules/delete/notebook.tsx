cc1709a132167491c1375feaf35f16ea395e89dd
hello30190f <108202763+hello30190f@users.noreply.github.com>
2025-10-03 10:03:04 +0900

    implement the frontend

--- firstPrototype/frontend/onenote alternative frontend/src/modules/delete/notebook.tsx
+++ firstPrototype/frontend/onenote alternative frontend/src/modules/delete/notebook.tsx
@@ -2,12 +2,23 @@ import { useReducer, useRef, useState } from "react";
 import { OverlayWindow, type OverlayWindowArgs } from "../UI/OverlayWindow";
 import { useToggleableStore, type toggleable } from "../UI/ToggleToolsBar";
 import { useStartButtonStore } from "../UI/ToggleToolsBar/StartButton";
+import { useAppState } from "../window";
 
 
 
 
 
 export function DeleteNotebook(){
+    const currentNotebook = useAppState((s) => s.currentNotebook)
+    const submitButtonBaseStyle = "submitbutton selection:bg-transparent mt-[1rem] p-[0.5rem] "
+    const [disabled,setDisabled] = useState(false)
+    let submitButtonStyle = submitButtonBaseStyle
+    if(disabled){
+        submitButtonStyle += " bg-gray-800 hover:bg-gray-900"    
+    }else{
+        submitButtonStyle += " bg-gray-800 hover:bg-gray-700"    
+    }
+
     // init -----------------------------------------
     // init -----------------------------------------
     const [visible,setVisible] = useState(false)
@@ -35,6 +46,14 @@ export function DeleteNotebook(){
 
 
     return <OverlayWindow arg={overlayWindowArg}>
-        <div className="m-[3rem]">Not Implemented yet...</div>
+        <div className="flex flex-col m-[0.5rem] min-w-[20rem]">
+            <div className="flex p-[0.5rem]">
+                <div className="mr-auto">Notebook: </div>
+                <div>{currentNotebook}</div>
+            </div>
+            <div className={submitButtonStyle}>
+                Delete the notebook
+            </div>
+        </div>
     </OverlayWindow>
 }
