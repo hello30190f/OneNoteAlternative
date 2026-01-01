cd57b9b58d0bb204ac6ef5bc7e3baf2ccf83c5db
hello30190f <108202763+hello30190f@users.noreply.github.com>
2026-01-01 14:43:27 +0900

    refactor showPingMessage job script on DataServer.

--- firstPrototype/backend/dataServer/jobs/showPingMessage.py
+++ firstPrototype/backend/dataServer/jobs/showPingMessage.py
@@ -2,12 +2,13 @@ import time
 
 def showPingMessage(): 
     print("observe scheduled task")
+    current = time.localtime()
     print("{:04d}/{:02d}/{:02d} {:02d}:{:02d}:{:02d}".format(
-        time.localtime().tm_year,
-        time.localtime().tm_mon,
-        time.localtime().tm_mday,
-        time.localtime().tm_hour,
-        time.localtime().tm_min,
-        time.localtime().tm_sec
+        current.tm_year,
+        current.tm_mon,
+        current.tm_mday,
+        current.tm_hour,
+        current.tm_min,
+        current.tm_sec
     ))
     print()
