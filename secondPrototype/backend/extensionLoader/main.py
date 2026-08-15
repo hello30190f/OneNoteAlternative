16941c0b25eda788ad0a3d813fd685b03dfe85cd
hello30190f <108202763+hello30190f@users.noreply.github.com>
2026-08-16 08:11:43 +0900

    write comment before implement

--- secondPrototype/backend/extensionLoader/main.py
+++ secondPrototype/backend/extensionLoader/main.py
@@ -1,10 +1,16 @@
 from dataserver     import init as dataserver
 from frontend       import init as frontend
 
+
+
 # init dataserver runtime
+# create import list (python), create array that hold all imported module. Then join the code into mainSys controllers
+# for runtime code, create "runtime" folder to store any "dynamic" code. (mainSys/controller/runtime mainSys/controller/hosting)
 dataserver()
 
 # init frontend runtime
+# create import list (TypeScript), create array that hold all imported module. Then join the code into the base frontend code.
+# for runtime code, create "runtime" folder to store any "dynamic" code. (frontend/runtime)
 frontend()
 
 
