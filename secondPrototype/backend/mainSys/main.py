81b2b7ba33fa89c3b1187bbc8d38819f3829af5e
hello30190f <108202763+hello30190f@users.noreply.github.com>
2026-08-26 08:13:18 +0900

    implement the dataServer hosting part

--- secondPrototype/backend/mainSys/main.py
+++ secondPrototype/backend/mainSys/main.py
@@ -7,7 +7,8 @@
 # Join the extensions code here END --------
 # Join the extensions code here END --------
 
-
+import multiprocessing
+import sys
 
 from controller.command     import init as command
 from controller.interrupt   import init as interrupt
@@ -17,23 +18,30 @@ from hosting.fileTransfar   import init as fileTransfar
 from hosting.frontendServe  import init as frontendServe
 
 # This array is "fixed". Not intended to be exntend by the extensions.
-controllers = [
-    command,
-    interrupt,
-    task
-]
-
-# This array is "fixed". Not intended to be exntend by the extensions.
 hosting = [
+    command,
+    task,
     fileTransfar,
     frontendServe
 ]
 
+# start each controllers and hosting service as a thread(or a process if it can be.)
+if __name__ == "__main__":
+    print("DataSrever is started")
+    processes = []
+    for host in hosting:
+        process = multiprocessing.Process(target=host)
+        process.start()
+        processes.append(process)
 
-# start each controllers as a thread(or a process if it can be.)
-
-
-
-# hosting service as a process
+    try:
+        for process in processes:
+            process.join()
+    except KeyboardInterrupt:
+        print("DataSrever will be stopped.")
+        for process in processes:
+            process.terminate()
+        print("All processes are terminated.")
+        sys.exit()
 
 
