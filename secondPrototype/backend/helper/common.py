f54b3a2d7b2b40e1ca0f01066f23866fb8d37a99
hello30190f <108202763+hello30190f@users.noreply.github.com>
2026-09-09 00:38:56 +0900

    write dataserver init script

--- secondPrototype/backend/helper/common.py
+++ secondPrototype/backend/helper/common.py
@@ -1,9 +1,36 @@
-DeafultSettings = """
-{
-    "backendBaseFolderPath": "/home/main/work/OneNoteAlternative/secondPrototype/backend"
-}
-"""
+import os.path, subprocess, pexpect, sys, time
 
-def generateDefaultSetting():
-    
+def generateDefaultSetting(Settings:dict):
+    # copy settings.deafult.json as settings.json
     pass
+
+
+
+def executeCommand(command:str):
+    response = subprocess.run([command,],shell=True,capture_output=True)
+    print(response.stdout.decode())
+    print("Exit: {}".format(response.returncode))
+
+def initVenv(Settings:dict) -> pexpect.spawn:
+    VenvPath = Settings["backendBaseFolderPath"] + "/dataServerVenv"
+    SourcePath = VenvPath + "/bin/python"
+
+    # create python venv if it does not exist
+    if(not os.path.exists(VenvPath)):
+        print("Prepare for python venv environment")
+        command = "cd {}; python -m venv dataServerVenv".format(Settings["backendBaseFolderPath"])
+        executeCommand(command)
+
+    # activate python venv (create terminal session)
+    dataServerSession = pexpect.spawn("/bin/bash")
+    dataServerSession.logfile = sys.stdout
+    # Give the terminal time to start
+    time.sleep(1)
+    pexpectExecuteCommand(dataServerSession,"source {}".format(SourcePath))
+    return dataServerSession
+
+def pexpectExecuteCommand(session:pexpect.spawn,command:str):
+    session.sendline(command)
+    # Give the command time to get executed
+    time.sleep(1)
+    session.expect(".+")
--- secondPrototype/backend/main.py
+++ secondPrototype/backend/main.py
@@ -3,23 +3,30 @@
 # This is the entry point for dataserver. Dataserver should be started with this script.
 # This is the entry point for dataserver. Dataserver should be started with this script.
 
-import json, sys
-import argparse
-import os.path
+import json, sys, os.path, pexpect, argparse
 from extensionLoader.main import loadExtension
 
-
-# TODO: separate extension load phase and mainSys start phase
-# python -u main.py --load-extension
-# python -u main.py --start-server
-
-
-# create python venv
-
-# activate python venv
+from helper.common import initVenv
 
 
 
+
+def loadExtensionOption(Settings:dict):
+    # Start python venv session
+    session:pexpect.spawn = initVenv(Settings)
+
+    # start extension loader
+    loadExtension(Settings)
+
+
+def startMainSystemOption(Settings:dict):
+    # execute mainSys/main.py by using subprocess lib.
+    pass
+
+
+
+# Load parmanent server settings ------------------
+# Load parmanent server settings ------------------
 # Check setting existance. When setting is not exist, DataServer will stop.
 Settings = None
 if(not os.path.exists("./settings.json")):
@@ -29,11 +36,33 @@ if(not os.path.exists("./settings.json")):
 # Load Server Settings
 with open("./settings.json") as settingFile:
     Settings = json.loads(settingFile.read())
+# Load parmanent server settings ------------------
+# Load parmanent server settings ------------------
 
 
-# start extension loader
-loadExtension(Settings)
 
 
 
-# execute mainSys/main.py by using subprocess lib.
+
+#NOTE: How to use this script
+# python -u main.py --loadExtension
+# python -u main.py --startServer
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--loadExtension")
+parser.add_argument("--startServer")
+
+args = parser.parse_args()
+if args.loadExtension and args.startServer:
+    print("--loadExtension and --startServer cannot be set at the same time.")
+    sys.exit(1)
+
+if not args.loadExtension and not args.startServer:
+    print("--loadExtension or --startServer isn't specified.")
+    sys.exit(1)
+
+if args.loadExtension: 
+    loadExtensionOption(Settings)
+
+if args.startServer:
+    startMainSystemOption(Settings)
